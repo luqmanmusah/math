2a5dbd249c2a0e5676af1ee63767473505cfeff6
Luqman Musah <musahluqman@gmail.com>
2021-10-20 12:40:35 +0000

    Add event Handlers

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,36 +9,47 @@ class Calculator extends React.Component {
       next: null,
       total: null
     };
+    this.Handler = this.Handler.bind(this);
   }
+  Handler(event) {
+    const { name } = event.target;
+    this.setState((state) => calculate(state, name));
+  }
+    render() {
+      const { next, total } = this.state;
+      return (
 
-    render = () => (
+     
+
       <div className="grid container max-width-sm">
-        <p value={0} className="result">0</p>
-        <button name="AC" type="button" className="button">AC</button>
-        <button name="+/-" type="button" className="button">+/-</button>
-        <button name="%" type="button" className="button">%</button>
-        <button name="/" type="button" className="operators">÷</button>
+        <Resul className="result" result={ next || ( total|| '0') }/>
+        <button onClick={this.Handler} name="AC" type="button" className="button">AC</button>
+        <button onClick={this.Handler} name="+/-" type="button" className="button">+/-</button>
+        <button onClick={this.Handler} name="%" type="button" className="button">%</button>
+        <button onClick={this.Handler} name="/" type="button" className="operators">÷</button>
 
-        <button name="7" type="button" className="button">7</button>
-        <button name="8" type="button" className="button">8</button>
-        <button name="9" type="button" className="button">9</button>
-        <button name="*" type="button" className="operators">×</button>
+        <button onClick={this.Handler} name="7" type="button" className="button">7</button>
+        <button onClick={this.Handler} name="8" type="button" className="button">8</button>
+        <button onClick={this.Handler} name="9" type="button" className="button">9</button>
+        <button onClick={this.Handler} name="*" type="button" className="operators">×</button>
 
-        <button name="4" type="button" className="button">4</button>
-        <button name="5" type="button" className="button">5</button>
-        <button name="6" type="button" className="button">6</button>
-        <button name="-" type="button" className="operators">-</button>
+        <button onClick={this.Handler} name="4" type="button" className="button">4</button>
+        <button onClick={this.Handler} name="5" type="button" className="button">5</button>
+        <button onClick={this.Handler} name="6" type="button" className="button">6</button>
+        <button onClick={this.Handler} name="-" type="button" className="operators">-</button>
 
-        <button name="1" type="button" className="button">1</button>
-        <button name="2" type="button" className="button">2</button>
-        <button name="3" type="button" className="button">3</button>
-        <button name="+" type="button" className="operators">+</button>
+        <button onClick={this.Handler} name="1" type="button" className="button">1</button>
+        <button onClick={this.Handler} name="2" type="button" className="button">2</button>
+        <button onClick={this.Handler} name="3" type="button" className="button">3</button>
+        <button onClick={this.Handler} name="+" type="button" className="operators">+</button>
 
-        <button name="0" type="button" id="zero" className="button">0</button>
-        <button name="." type="button" className="button">.</button>
-        <button name="=" type="button" className="operators">=</button>
+        <button onClick={this.Handler} name="0" type="button" id="zero" className="button">0</button>
+        <button onClick={this.Handler} name="." type="button" className="button">.</button>
+        <button onClick={this.Handler} name="=" type="button" className="operators">=</button>
       </div>
     );
+    
+  }
 }
 
 export default Calculator;
