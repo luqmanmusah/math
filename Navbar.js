48cb1cbfd08ea7d3f96a901c00a9fefa8d516507
Luqman Musah <musahluqman@gmail.com>
2021-10-27 17:30:22 +0000

    Add Navbar and links

---
+++ Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/Calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/Quotes',
+      text: 'Quotes',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <h1>Maths Magician</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              exact
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
