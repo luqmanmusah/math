1c19d85a676567f81b4f581e9770afb06556c40e
Luqman Musah <musahluqman@gmail.com>
2021-10-27 17:27:56 +0000

    Add pages folder

---
+++ src/pages/Quotes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quotes = () => (
+  <div className="quote-message">
+    <h3>
+      Without mathematics, there’s nothing you can do.
+      Everything around you is mathematics.
+      Everything around you is numbers.
+      — Shakuntala Devi, Indian writer and mental calculator
+    </h3>
+  </div>
+);
+export default Quotes;
